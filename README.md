48b768304288c206bd61c4d9f08ffa5aeb85fc7d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-01 08:34:54 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Here are the steps I took to create this project.
+
+- `cddev`
+- `cd htmx`
+- `mkdir htmx-flask`
+- `cd htmx-flask`
+- `python -m venv .venv`
+- `. .venv/bin/activate`
+- `pip install Flask`
+- `pip install requests`
+- Create the file `server.py`.
+- Start the server by entering `./run`
+- Browse localhost:5000
---
+++ app.py
@@ -0,0 +1,109 @@
+#!/bin/python
+
+from flask import Flask, redirect, render_template, request, Response
+import uuid
+
+dog_map = {}
+
+selected_id = '';
+
+def add_dog(name, breed):
+    id = str(uuid.uuid4())
+    dog = {'id': id, 'name': name, 'breed': breed}
+    dog_map[id] = dog
+    return dog
+
+add_dog('Comet', 'Whippet')
+add_dog('Oscar', 'German Shorthaired Pointer')
+
+def dog_row(_dog, updating):
+  _attrs = {};
+  if updating:
+     _attrs['hx-swap-oob'] = 'true';
+  return render_template('dog_row.html', dog=_dog, swap=updating)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/dogs')
+
+@app.route('/dogs')
+def all_dogs():
+    return render_template('dogs.html')
+
+@app.route('/dogs/<id>')
+def one_dog(id):
+    # Parameter names and values cannot match (ex. cannot have dogs=dogs).
+    _dog = dog_map[id]
+    return render_template(
+        'dog.html',
+        title=_dog['name'],
+        dog=_dog
+    )
+
+# Deletes the dog with a given id.
+@app.route('/dog/<id>', methods=['DELETE'])
+def delete_dog(id):
+    del dog_map[id]
+    return ''
+
+# Deselects the currently selected dog.
+@app.route('/deselect')
+def deselect():
+    global selected_id
+    selected_id = ''
+    res = Response('')
+    res.headers['HX-Trigger'] = 'selection-change'
+    return res
+
+# Gets the proper form for either adding or updating a dog.
+@app.route('/form')
+def form():
+    _dog = dog_map.get(selected_id)
+    return render_template(
+        'form.html',
+        id=selected_id,
+        dog=_dog
+    )
+
+# Gets table rows for all the dogs.
+@app.route('/rows')
+def rows():
+    sorted_dogs = sorted(dog_map.values(), key=lambda x: x['name'])
+    print('sorted_dogs:', sorted_dogs)
+    return render_template('dog-rows.html', dogs=sorted_dogs)
+
+# Selects a dog.
+@app.route('/select/<id>')
+def select(id):
+    global selected_id
+    selected_id = id;
+    res = Response('')
+    res.headers['HX-Trigger'] = 'selection-change'
+    return res
+
+# Creates a dog.
+@app.route('/dog', methods=['POST'])
+def create():
+    name = request.form.get('name')
+    breed = request.form.get('breed')
+    _dog = add_dog(name, breed);
+    return render_template('dog-row.html', dog=_dog, status=201);
+
+# Updates a dog
+@app.route('/dog/<id>', methods=['PUT'])
+def update(id):
+    name = request.form.get('name')
+    breed = request.form.get('breed')
+    updatedDog = {id, name, breed};
+
+    global dogs
+    dogs[id] = updatedDog;
+
+    global selectedId
+    selectedId = '';
+
+    res = Response(dog_row(updatedDog, True))
+    res.headers['HX-Trigger'] = 'selection-change'
+    return res
